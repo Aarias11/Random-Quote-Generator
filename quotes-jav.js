b0950436a0027aea2592bfadc87a41b32ff53e2a
Aarias11 <108088290+Aarias11@users.noreply.github.com>
2023-02-02 14:09:10 -0500

    Add files via upload

---
+++ quotes-jav.js
@@ -0,0 +1,149 @@
+let quote = document.getElementById('quote');
+let author = document.getElementById('author');
+let btn = document.getElementById('btn');
+
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'dbf6f80d93msh81cd79abc88ec01p124b8fjsn8506d3f70bc0',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+	}
+};
+
+document.getElementById('btn').addEventListener('click', nextQuote)
+
+function nextQuote() {
+
+fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+	.then(response => response.json())
+	.then(data => {
+        console.log(data)
+        document.getElementById('quote').innerHTML = `"${data.content}"`
+        document.getElementById('author').innerHTML = `- ${data.originator.name}`
+    })
+	.catch(err => console.error(err));
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let quote = document.getElementById('quote');
+// let author = document.getElementById('author');
+// let next = document.getElementById('next');
+
+
+
+
+//  const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': 'dbf6f80d93msh81cd79abc88ec01p124b8fjsn8506d3f70bc0',
+//         'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+//     }
+// };
+
+
+// document.querySelector('#next').addEventListener('click', newQuote)
+
+// function newQuote() {
+
+// fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//         document.querySelector('p').innerHTML = data.content
+//         document.querySelector('span').innerHTML = data.originator.name
+        
+//     })
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let quote = document.getElementById('quote');
+// let author = document.getElementById('author');
+// let next = document.getElementById('next');
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'dbf6f80d93msh81cd79abc88ec01p124b8fjsn8506d3f70bc0',
+// 		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+// 	}
+// };
+
+
+// document.querySelector('#next').addEventListener('click', newQuote)
+
+// function newQuote() {
+// fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+// 	.then(response => response.json())
+// 	.then(data => {
+//         console.log(data.content)
+//         document.querySelector('p.quote').innerHTML = data.content
+//         document.querySelector('span').innerHTML = data.originator.name
+        
+        
+
+//     })
+// 	.catch(err => console.error(err));
+// }
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+    
